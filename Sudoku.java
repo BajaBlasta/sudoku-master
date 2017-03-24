8695a3d2c4460de43d148dfb4d5102228ef92479
schiebra <bradleyschies@gmail.com>
2017-03-24 01:22:41 -0400

    Add files via upload
    https://images5.alphacoders.com/423/423148.jpg

---
+++ Sudoku.java
@@ -0,0 +1,11 @@
+
+public class Sudoku {
+	public static void main(String[] args) {
+		
+		int difficulty = 3; //set default difficulty to easy
+		Game game = new Game(difficulty);
+		
+		System.out.println("WHAT IS GOING ON HERE"); //a print statement
+		
+	}
+}
