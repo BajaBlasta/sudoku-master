9192e0e55cddce14adb41c3f1b564f8d471db0de
TheMarker-1 <deadlimbgaming@gmail.com>
2017-03-30 14:05:48 -0400

    Update Sudoku.java
    Changed constructor to build a new Sudoku with a timer that starts at 0 and counts up

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,7 +3,8 @@ public class Sudoku {
 	public static void main(String[] args) {
 		
 		int difficulty = 3; //set default difficulty to easy
-		Game game = new Game(difficulty);
+		boolean normalTimer = true;	//sets the timer to count up, instead of down
+		Game game = new Game(difficulty, normalTimer);
 		
 		System.out.println("WHAT IS GOING ON HERE"); //a print statement
 		
