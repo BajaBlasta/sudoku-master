d628b1a07eb381f7ee0f698ed9b8f4c063f1de22
AaronRiegel <riegelaaron@gmail.com>
2017-03-31 13:29:58 -0400

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,10 +1,11 @@
 
 public class Sudoku {
 	public static void main(String[] args) {
+		StartPage startpage = new StartPage();
 		
-		int difficulty = 3; //set default difficulty to easy
-		boolean normalTimer = true;	//sets the timer to count up, instead of down
-		Game game = new Game(difficulty, normalTimer);
+		//int difficulty = 3; //set default difficulty to easy
+		//boolean normalTimer = true;	//sets the timer to count up, instead of down
+		//Game game = new Game(difficulty, normalTimer);
 		
 		System.out.println("WHAT IS GOING ON HERE"); //a print statement
 		
