93ad214ca1711ce9168fdd48c4fc6f586e8f0a04
AaronRiegel <riegelaaron@gmail.com>
2017-04-07 13:28:26 -0400

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,7 +1,7 @@
+import java.io.IOException;
 
 public class Sudoku {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
 		
 		//boolean normalTimer = true;	//sets the timer to count up, instead of down- still needs to be implemented in StartPage
 		StartPage startPage = new StartPage(); //create new Start Page
