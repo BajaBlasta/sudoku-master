599ca5d14fdaa68c84f979c7e99d3c62cb49bcfb
AaronRiegel <riegelaaron@gmail.com>
2017-04-06 22:46:32 -0400

    Add code to support StartPage functionality

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -2,9 +2,9 @@
 public class Sudoku {
 	public static void main(String[] args) {
 		
-		int difficulty = 3; //set default difficulty to easy
-		boolean normalTimer = true;	//sets the timer to count up, instead of down
-		Game game = new Game(difficulty, normalTimer);
+		
+		//boolean normalTimer = true;	//sets the timer to count up, instead of down- still needs to be implemented in StartPage
+		StartPage startPage = new StartPage(); //create new Start Page
 		
 		System.out.println("WHAT IS GOING ON HERE"); //a print statement
 		
