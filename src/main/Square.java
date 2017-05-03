a725ee8517bb022b103efa2476a44a86984e6dee
TheMarker-1 <deadlimbgaming@gmail.com>
2017-05-03 15:00:59 -0400

    Update Square.java
    Fixed warnings

--- src/main/Square.java
+++ src/main/Square.java
@@ -1,7 +1,5 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -11,6 +9,7 @@ import javax.swing.JToggleButton;
 
 public class Square extends JToggleButton {
 
+	private static final long serialVersionUID = 1L;
 	private boolean isLocked; //variable is set to true if value inside square was generated
 	private short value; //current number inside square
 	private Square[][] board; //reference to all other squares on the board
